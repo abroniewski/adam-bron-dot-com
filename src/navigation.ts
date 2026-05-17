32a69d3504771095fac53177022d7acf6ddd2956
Adam Broniewski <abroniewski@gmail.com>
2026-05-18 01:00:40 +0200

    feat: add projects page and update navigation with projects link

--- src/navigation.ts
+++ src/navigation.ts
@@ -3,6 +3,7 @@ import { getAsset, getHomePermalink, getPermalink } from './utils/permalinks';
 export const headerData = {
   links: [
     { text: 'Home', href: getHomePermalink() },
+    { text: 'Projects', href: getPermalink('/projects') },
     { text: 'The Lab', href: getPermalink('/the-lab') },
   ],
   actions: [],
